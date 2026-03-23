79e400c4b4856933d2c9152c3623f4fae345ea84
Jordan <jordantw128@gmail.com>
2026-03-22 22:01:23 -0400

    .

--- proj2/src/proj2_server.cc
+++ proj2/src/proj2_server.cc
@@ -6,8 +6,17 @@
 #include "thread_log.h"
 #include "timings.h"
 
+#include <pthread.h>
+#include <semaphore.h>
+
 #include <vector>
 
+void* StartRoutine(void* arg);
+
 int main(int argc, char* argv[]) {
+    
+}
+
+void* StartRoutine(void* arg){
 
 }
