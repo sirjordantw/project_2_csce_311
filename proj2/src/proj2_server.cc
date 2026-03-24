16941ba82b30a68242dadbb9b9dc494b64178180
sirjordantw <jordantw128@gmail.com>
2026-03-24 11:00:52 -0400

    .

--- proj2/src/proj2_server.cc
+++ proj2/src/proj2_server.cc
@@ -7,9 +7,18 @@
 #include "timings.h"
 
 #include <pthread.h>
-#include <semaphore.h>
+#include <csignal>
 
 #include <vector>
+
+void ParseMessage(const std::string& msg,
+    std::string* client_addr,
+    std::vector<std::string>* file_paths,
+    std::vector<std::uint32_t>* rows_per_file) {
+        const char* c_ptr = msg.data();
+}
+
+std::sig_atomic_t term = 1;
 
 void* StartRoutine(void* arg);
 
